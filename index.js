2c307864227d907936daaacf68b48588433077eb
kunwar108 <abhaychauhan9555@gmail.com>
2026-05-27 14:39:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get("/", (req, res) => {
   res.send("Earnify X Server Running 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
